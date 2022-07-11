cf72f2a1a34c610eccd794971a9ddfb70f9423c3
Osmar Araujo Garção <araujooa@gmail.com>
2022-07-11 20:45:27 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div className="dsmeta-logo-container">
+                    <img src={logo} alt="DSMeta" />
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por Osmar Araujo Garção
+                        <a href="https://github.com/Osmar-Araujo"> @GitHub</a>
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
